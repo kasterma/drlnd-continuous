1dd9c125c2d442ca77e35a7dcaeaac1cfc5e683b
Bart Kastermans <kasterma@kasterma.net>
2018-12-09 16:06:27 +0100

    fix: remove warning about wrong tanh

--- drlnd_continuous/model.py
+++ drlnd_continuous/model.py
@@ -53,7 +53,7 @@ class Actor(nn.Module):
         """Build an actor (policy) network that maps states -> actions."""
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
-        return F.tanh(self.fc3(x))
+        return torch.tanh(self.fc3(x))
 
     def get_copy(self) -> 'Actor':
         copy = Actor(self.state_size, self.action_size, self.fc1_units, self.fc2_units)
